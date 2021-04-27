80146fcba29e2fb1aacb3b8b55a62969e52a9848
Fábio Silva <f.m.g.silva@hotmail.com>
2021-04-27 16:22:57 +0100

    Added layout for sent and received images

--- app/src/main/java/pt/up/fc/progmovel/socialapp/ui/chat/ChatInputFragment.java
+++ app/src/main/java/pt/up/fc/progmovel/socialapp/ui/chat/ChatInputFragment.java
@@ -1,5 +1,6 @@
 package pt.up.fc.progmovel.socialapp.ui.chat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.socialapp.R;
 import java.util.Calendar;
@@ -18,6 +20,7 @@ public class ChatInputFragment extends Fragment {
     private ChatMessage mChatMessage;
     private EditText inputMessage;
     private ImageButton sendButton;
+    private ImageButton imagesButton;
 
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -30,10 +33,24 @@ public class ChatInputFragment extends Fragment {
 
         inputMessage = view.findViewById(R.id.chat_message_input);
         sendButton = view.findViewById(R.id.send_image_button);
+        imagesButton = view.findViewById(R.id.image_input_button);
+
+        imagesButton.setOnClickListener(new ImagesButtonClickListener());
 
         return view;
     }
 
+    private class ImagesButtonClickListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            intent.setType("image/*");
+            startActivity(intent);
+
+        }
+    }
 
 
 }
